ec2e9f4a19512c020f95b879e16ddd1262f9a9ce
Nadyl Driessen <nadyl9@live.nl>
2024-10-02 12:08:52 +0200

    extra aanpassingen

--- resources/js/Components/YourBet.jsx
+++ resources/js/Components/YourBet.jsx
@@ -4,14 +4,22 @@ import React from "react";
 
 export default function YourBet() {
     return (
-        <>
-            <div className="items-center space-x-2 bg-scamdom-40 rounded-lg px-4 py-2">
+            <div className="items-center space-x-2 bg-scamdom-40 rounded-lg px-4 py-2 flex justify-center">
                 <label className="text-gray-400 text-xs">Your bet</label>
                 <div className="flex items-center space-x-1 text-white">
-                    <input placeholder={"0.00"} className="bg-scamdom-30 focus:ring-transparent focus:border-scamgreen-30  rounded-xl border-transparent transition duration-500 hover:border-scamgreen-30"/>
-                    <button className="bg-scamgreen-50">+$10</button>
+                    <div className="relative">
+                        <input placeholder={"0.00"}
+                               className="bg-scamdom-30 focus:ring-transparent focus:border-scamgreen-30  rounded-xl border-transparent transition duration-500 hover:border-scamgreen-30">
+                        </input>
+                        <button className="absolute bg-scamdom-40 rounded-xl right-1 top-1/2 -translate-y-1/2 px-3 py-1.5 hover:text-scamgreen-30 transition duration-500">Clear</button>
+                    </div>
+                    <button className="bg-scamgreen-50 px-12 h-10 rounded-md text-scamgreen-40"><h6>+$10</h6></button>
+                    <button className="bg-scamgreen-50 px-12 h-10 rounded-md text-scamgreen-40"><h6>+$50</h6></button>
+                    <button className="bg-scamgreen-50 px-12 h-10 rounded-md text-scamgreen-40"><h6>+$100</h6></button>
+                    <button className="bg-scamgreen-50 px-12 h-10 rounded-md text-scamgreen-40"><h6>1/2</h6></button>
+                    <button className="bg-scamgreen-50 px-12 h-10 rounded-md text-scamgreen-40"><h6>x2</h6></button>
+                    <button className="bg-scamgreen-50 px-12 h-10 rounded-md text-scamgreen-40"><h6>Max</h6></button>
                 </div>
             </div>
-        </>
     )
 }
