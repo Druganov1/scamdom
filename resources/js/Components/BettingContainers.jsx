f90dccb788de00183297c537b88491cdf169b5f7
Nadyl Driessen <nadyl9@live.nl>
2024-10-03 21:34:27 +0200

    responsive betting

--- resources/js/Components/BettingContainers.jsx
+++ resources/js/Components/BettingContainers.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 export function BettingContainers() {
     return (
-        <div className="justify-center flex space-x-5">
-            <div className="space-x-2 bg-scamdom-40 rounded-xl py-40">
-                <button className="text-white px-52 bg-roulette-red">1 to 7</button>
+        <div className="justify-center flex space-x-5 flex-col md:flex-row">
+            <div className="space-x-2 bg-scamdom-40 rounded-xl">
+                <button className="text-white bg-roulette-red">1 to 7</button>
             </div>
-            <div className="space-x-2 bg-scamdom-40 rounded-xl py-40 ">
-                <button className="text-white bg-roulette-green px-52">0</button>
+            <div className="space-x-2 bg-scamdom-40 rounded-xl ">
+                <button className="text-white bg-roulette-green">0</button>
             </div>
-            <div className="space-x-2 bg-scamdom-40 rounded-xl py-40">
-                <button className="bg-gray-500 text-white px-52">8 to 14</button>
+            <div className="space-x-2 bg-scamdom-40 rounded-xl">
+                <button className="bg-gray-500 text-white">8 to 14</button>
             </div>
         </div>
     )
--- resources/js/Components/YourBet.jsx
+++ resources/js/Components/YourBet.jsx
@@ -4,22 +4,45 @@ import React from "react";
 
 export default function YourBet() {
     return (
-            <div className="items-center space-x-2 bg-scamdom-40 rounded-lg py-2 flex justify-center grid-cols-5">
-                <label className="text-gray-400 text-xs">Your bet</label>
-                <div className="flex items-center space-x-1 text-white">
-                    <div className="relative">
-                        <input placeholder={"0.00"}
-                               className="bg-scamdom-30 focus:ring-transparent focus:border-scamgreen-30  rounded-xl border-transparent transition duration-500 hover:border-scamgreen-30">
-                        </input>
-                        <button className="absolute bg-scamdom-40 rounded-xl right-1 top-1/2 -translate-y-1/2 px-3 py-1.5 hover:text-scamgreen-30 transition duration-500">Clear</button>
-                    </div>
-                    <button className="bg-scamgreen-50 px-12 h-10 rounded-md text-scamgreen-40"><h6>+$10</h6></button>
-                    <button className="bg-scamgreen-50 px-12 h-10 rounded-md text-scamgreen-40"><h6>+$50</h6></button>
-                    <button className="bg-scamgreen-50 px-12 h-10 rounded-md text-scamgreen-40"><h6>+$100</h6></button>
-                    <button className="bg-scamgreen-50 px-12 h-10 rounded-md text-scamgreen-40"><h6>1/2</h6></button>
-                    <button className="bg-scamgreen-50 px-12 h-10 rounded-md text-scamgreen-40"><h6>x2</h6></button>
-                    <button className="bg-scamgreen-50 px-12 h-10 rounded-md text-scamgreen-40"><h6>Max</h6></button>
+        <div className="bg-scamdom-40 rounded-lg py-4 flex max-md:flex-wrap ">
+            <div className="w-full flex-wrap justify-center flex items-center md:flex-row md:justify-center md:space-x-4">
+                <label className="text-gray-400 text-xs mb-2 md:mb-0">Your bet</label>
+
+                <div className="relative w-full md:max-w-xs max-md:mb-5 max-md:mx-3">
+                    <input
+                        placeholder="0.00"
+                        className="w-full text-white bg-scamdom-30 focus:ring-transparent focus:border-scamgreen-30 rounded-xl border-transparent transition duration-500 hover:border-scamgreen-30 px-4 py-2"
+                    />
+                    <button
+                        className="absolute bg-scamdom-40 rounded-xl right-2 top-1/2 -translate-y-1/2 px-3 py-1.5 text-white hover:text-scamgreen-30 transition duration-500"
+                    >
+                        Clear
+                    </button>
                 </div>
             </div>
+
+            <div
+                className="grid grid-cols-3 gap-2 w-full px-4 lg:px-0 lg:justify-center lg:items-center lg:flex lg:flex-wrap">
+                <button className="bg-scamgreen-50 px-6 py-2 rounded-md text-scamgreen-40">
+                    +$10
+                </button>
+                <button className="bg-scamgreen-50 px-6 py-2 rounded-md text-scamgreen-40">
+                    +$50
+                </button>
+                <button className="bg-scamgreen-50 px-6 py-2 rounded-md text-scamgreen-40">
+                    +$100
+                </button>
+                <button className="bg-scamgreen-50 px-6 py-2 rounded-md text-scamgreen-40">
+                    1/2
+                </button>
+                <button className="bg-scamgreen-50 px-6 py-2 rounded-md text-scamgreen-40">
+                    x2
+                </button>
+                <button className="bg-scamgreen-50 px-6 py-2 rounded-md text-scamgreen-40">
+                    Max
+                </button>
+            </div>
+        </div>
+
     )
 }
